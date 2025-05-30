e5593b0a72f83c14be470dbcdaeb11d7e0df1c32
ANTONIO RAMOS LOPES <antonio.rlopes1@senacsp.edu.br>
2025-05-30 16:12:32 -0300

    mario bros2

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
+
 const openModalBtn = document.getElementById('openModalBtn');
 const modal = document.getElementById('modal');
 const closeBtn = modal.querySelector('.close-btn');
-const youtubeVideo = document.getElementById('youtubeVideo');
 
-// Link do vídeo do YouTube (embed) com autoplay
-const videoURL = "https://www.youtube.com/embed/TnGl01FkMMo?autoplay=1";
 
 openModalBtn.addEventListener('click', () => {
-  youtubeVideo.src = videoURL; // coloca o vídeo e inicia autoplay
-  modal.style.display = 'flex';
+  modal.style.display = 'block';
 });
 
+
 function closeModal() {
+  const iframe = modal.querySelector('iframe');
+  iframe.src = iframe.src;
   modal.style.display = 'none';
-  youtubeVideo.src = ""; // remove o src para parar o vídeo
 }
 
 closeBtn.addEventListener('click', closeModal);
-
-// Fechar clicando fora do modal-content
-window.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    closeModal();
-  }
-});
